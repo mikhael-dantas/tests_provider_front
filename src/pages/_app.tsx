923a52c655ac76b9876866414f0ab8b05f83d0f2
mikhael-dantas <mikhael-dcs@outlook.com>
2022-08-09 13:11:24 -0300

    initial setup from brainstorm

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+import theme from '../theme'
+import { AppProps } from 'next/app'
+import '../style.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,571 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+  Flex,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import React from 'react';
+
+interface IUcfrItem {
+  id: string,
+  done: boolean,
+  name: string,
+  dependencies: string[]
+}
+
+interface IUcfrs {
+  ucfrs: {
+      category: string,
+      type: string,
+      list: IUcfrItem[]
+  }[], 
+  newUcfr: string,
+  newItem: string,
+  newDependency: string
+}
+
+export default function Index() {
+  // make component with a list of drag and drop writable components
+
+  const [ucfrs, setUcfrs] = React.useState<IUcfrs>({
+    ucfrs: [
+          {
+          category: 'Manager',
+          type: 'UC',
+          list: []
+        },
+    ],
+    newUcfr: '',
+    newItem: '',
+    newDependency: ''
+
+  })
+
+  const [currentUcfrs, setCurrentUcfrs]  = React.useState({
+    current: {
+        category: 'Manager',
+        type: 'UC',
+    },
+  })
+
+  const [jsonInsert, setJsonInsert ] = React.useState('')
+
+
+  const generateUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+
+  // drag
+  const [dragItem, setDragItem] = React.useState(null);
+  
+  const onDragStart = (e, item) => {
+    setDragItem(item);
+  }
+  const onDragEnd = (e) => {
+    setDragItem(null);
+  }
+  const onDragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+  }
+  const onDrop = (e, id) => {
+    // make the logic for the current category
+    // the drag item should exit the list, then on drop it should be added to the list moving the forward elements to the right
+    if (dragItem) {
+        const fUcfr = ucfrs.ucfrs.find(current => current.category === currentUcfrs.current.category && current.type === currentUcfrs.current.type)
+        if (!fUcfr) { throw new Error("Could not find the current category")}
+        const newItems = [...fUcfr.list];
+        const dragItemIndex = newItems.findIndex(current => current.id === dragItem['id']);
+        const dropItemIndex = newItems.findIndex(current => current.id === id);
+        newItems.splice(dragItemIndex, 1);
+        newItems.splice(dropItemIndex, 0, dragItem);
+        setUcfrs({
+          ...ucfrs,
+          ucfrs: [...ucfrs.ucfrs.map(current => current.category === currentUcfrs.current.category && current.type === currentUcfrs.current.type ? {
+              ...current,
+              list: newItems
+          } : current)]
+        })
+    }
+  }
+
+
+
+
+
+  const currentUcfrsHandlerSelect = (e) => {
+    const [type, category] = e.target.value.split('-')
+    setCurrentUcfrs({
+        current: {
+          category,
+          type,
+        },
+    })
+  }
+  const addUcfr = () => {
+
+    const [type, category] = ucfrs.newUcfr.split('-')
+
+    if(type !== 'UC' && type !== 'FR') {
+        alert('Please select a valid type')
+        return;
+    }
+    // check if the category with the same type already exists in model
+    const exists = ucfrs.ucfrs.find(compare => compare.category === category && compare.type === type);
+    if (exists) {
+        alert('Business Model Model already exists');
+        return;
+    }
+
+    setUcfrs({
+        ucfrs: [...ucfrs.ucfrs, {
+          category,
+          type,
+          list: []
+        }],
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    })
+  }
+  const delUcfr = () => {
+    const [type, category] = ucfrs.newUcfr.split('-')
+    // check if current tab is attempting to be removed
+    if(currentUcfrs.current.category === category && currentUcfrs.current.type === type) {
+        alert('Cannot remove current tab')
+        return;
+    }
+    // check if items is empty before removing
+    const fUcfrs = ucfrs.ucfrs.find(compare => compare.category === category && compare.type === type)
+    if(!fUcfrs) { throw new Error('Could not find the current category')}
+    const items = fUcfrs.list;
+    if (items.length !== 0) {
+        alert('Cannot remove tab with items')
+        return;
+    }
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.filter((_, i) => _.category !== category || _.type !== type),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    })
+  }
+  const handleChangeUCFRInput = (e) => {
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs,
+        newUcfr: e.target.value,
+        newItem: '',
+        newDependency: ''
+    });
+  }
+
+
+
+
+
+  const addItem = () => {
+    // add ucfr item to current ucfr category
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: [...ucfr.list, {
+                    id: `${generateUUID()}`,
+                    done: false,
+                    name: ucfrs.newItem,
+                    dependencies: []
+                }]
+              }
+          }
+          return ucfr;
+        }, []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+  const editItemViaAlertInput = (id: string) => {
+  // edit item name via alert input
+  const fUcfr = ucfrs.ucfrs.find(ucfr => ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type)
+  if (!fUcfr) { throw new Error("Could not find the current category")}
+  const item = fUcfr.list.find(item => item.id === id);
+  if (!item) {
+    alert('Item not found');
+    return;
+  }
+  const newName = prompt('Enter new name', item.name);
+  if (newName) {
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: ucfr.list.map(item => item.id === id ? {
+                    ...item,
+                    name: newName
+                } : item)
+              }
+          }
+          return ucfr;
+        }
+        , []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+}
+  const removeItem = (id: string) => {
+    // remove item from current ucfr category
+    // cannot remove item if it is dependency of other item
+
+    let isDependency = false
+    let dependencyName = ''
+    for (const ucfr of ucfrs.ucfrs) {
+        for (const item of ucfr.list) {
+          if (item.dependencies.includes(id)) {
+              isDependency = true;
+              dependencyName = item.name;
+          }
+        }
+    }
+
+    if (isDependency) {
+        alert('Cannot remove item because it is dependency of ' + dependencyName);
+        return;
+    }
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: ucfr.list.filter(item => item.id !== id)
+              }
+          }
+          return ucfr;
+        }
+        , []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+  const toggleItem = (id: string) => {
+    // toggle item from current ucfr category
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: ucfr.list.map(item => {
+                    if (item.id === id) {
+                      return {
+                          ...item,
+                          done: !item.done
+                      }
+                    }
+                    return item;
+                })
+              }
+          }
+          return ucfr;
+        }
+        , []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+  const handleChange = (e) => {
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs,
+        newUcfr: ucfrs.newUcfr,
+        newItem: e.target.value,
+        newDependency: ucfrs.newDependency
+    });
+  }
+
+
+  const findDependencyName = (itemId: string) => {
+    for (const ucfr of ucfrs.ucfrs) {
+        for (const item of ucfr.list) {
+          const found = item.id === itemId;
+          if (found) {
+              return ucfr.type + '-' + ucfr.category + '| ' + item.name
+          }
+        }
+    }
+    return 'invalid';
+  }
+
+  const addDependency = (itemId: string, dependencyItemId: string) => {
+    if(dependencyItemId === "null") {
+        return
+    }
+    // check if item is already in the dependency list
+    let isDependency = false;
+    for (const ucfr of ucfrs.ucfrs) {
+        for (const item of ucfr.list) {
+          if (item.id === itemId) {
+              if (item.dependencies.includes(dependencyItemId)) {
+                isDependency = true;
+              }
+          }
+        }
+    }
+    if (isDependency) {
+        alert('Item is already a dependency');
+        return;
+    }
+    // add dependency to current ucfr itemId dependency list
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: ucfr.list.map(item => { if (item.id === itemId) {
+                      return {
+                          ...item,
+                          dependencies: [...item.dependencies, dependencyItemId]
+                      }
+                    }
+                    return item;
+                })
+              }
+          }
+          return ucfr;
+        }
+        , []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+
+  const removeDependency = (itemId: string, dependencyItemId: string) => {
+    if(dependencyItemId === "null") {
+        return
+    }
+    // check if item is in fact in the dependency list
+    let isDependency = false;
+    for (const ucfr of ucfrs.ucfrs) {
+        for (const item of ucfr.list) {
+          if (item.id === itemId) {
+              if (item.dependencies.includes(dependencyItemId)) {
+                isDependency = true;
+              }
+          }
+        }
+    }
+    if (!isDependency) {
+        alert('Item is not a dependency');
+        return;
+    }
+    // remove dependency from current ucfr itemId dependency list
+    setUcfrs({
+        ucfrs: ucfrs.ucfrs.map(ucfr => {
+          if (ucfr.category === currentUcfrs.current.category && ucfr.type === currentUcfrs.current.type) {
+              return {
+                ...ucfr,
+                list: ucfr.list.map(item => { if (item.id === itemId) {
+                      return {
+                          ...item,
+                          dependencies: item.dependencies.filter(dependency => dependency !== dependencyItemId)
+                      }
+                    }
+                    return item;
+                })
+              }
+          }
+          return ucfr;
+        }
+        , []),
+        newUcfr: '',
+        newItem: '',
+        newDependency: ''
+    });
+  }
+
+  const saveUcfrsInLocalStorage = () => {
+  localStorage.setItem('ucfrs', JSON.stringify(ucfrs));
+  alert('saved')
+}
+
+const loadUcfrsFromLocalStorage = () => {
+    const ucfrsFromLocalStorage = localStorage.getItem('ucfrs');
+    if (ucfrsFromLocalStorage) {
+    setUcfrs(JSON.parse(ucfrsFromLocalStorage));
+    } else {
+    alert('No ucfrs found in local storage');
+    }
+    alert('loaded')
+}
+
+const downloadUcfrsAsJson = () => {
+    const json = JSON.stringify(ucfrs);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'ucfrs.json';
+    link.click();
+    alert('download should start now')
+}
+
+const insertucfrsFromJson = () => {
+  let jsontomakeinsertion
+  try {
+    jsontomakeinsertion = JSON.parse(jsonInsert);
+    setUcfrs(jsontomakeinsertion)
+  } catch (error) {
+    alert('Invalid JSON');
+    return
+  }
+  // check if json have all properties that ucfrs have
+  Object.keys(jsontomakeinsertion).forEach(key => {
+    if (!ucfrs.hasOwnProperty(key)) {
+        alert('Invalid json');
+        return;
+    }
+  })
+  alert('ucfrs inserted');
+}
+
+return (
+  <div className="App">
+    <div className="card" 
+    // blue background and 100% width
+    style={{
+    width: '100%',
+    display: "flex",
+    flexDirection: "column",
+    }}>
+    <div className="change-tab"
+        style={{
+        
+    }}>
+        <select onChange={currentUcfrsHandlerSelect}>
+        {ucfrs.ucfrs.map((model, i) => (
+        <option key={i} value={`${model.type}-${model.category}`}>{model.type}-{model.category}</option>
+        ))}
+        </select>
+        <h1>{currentUcfrs.current.category}</h1>
+        <div className="ucfr-insertion"
+          style={{
+          display: "flex",
+          flexDirection: "column"
+        }}>
+          <input type="text" value={ucfrs.newUcfr} onChange={handleChangeUCFRInput} />
+          <button onClick={addUcfr}>Add Item</button>
+          <button onClick={delUcfr}>Remove</button>
+        </div>
+    </div>
+
+
+    <div className="sub-card"
+    style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '80%',
+        margin: '0 auto',
+        marginTop: '1rem'
+
+    }}>
+        <h3>Drag and Drop UCFRs</h3>
+
+        <div className="item-insertion">
+          <input type="text" value={ucfrs.newItem} onChange={handleChange} />
+          <button onClick={addItem}>Add Item</button>
+        </div>
+
+        <div className="card-body"
+        style={{
+          width: '90%'
+        }}>
+          {/* list items from the current category */}
+          {(ucfrs.ucfrs.find(model => model.category === currentUcfrs.current.category && model.type === currentUcfrs.current.type) as any)
+          .list.map(item => (
+          <div key={item.id} className="col-md-3"
+          style={{
+              // background: '#ccc',
+              margin: '10px',
+          }}>
+              <div className="drag-item"
+                style={{ 
+                
+                }}
+                draggable
+                onDragStart={(e) => onDragStart(e, item)}
+                onDragEnd={onDragEnd}
+                onDragOver={onDragOver}
+                onDrop={(e) => onDrop(e, item.id)}
+              >
+                <div className='item-ucfr'
+                style={{
+                }}>
+                <input type="checkbox" checked={item.done} onChange={() => toggleItem(item.id)} />
+                {item.name}
+                <button onClick={() => removeItem(item.id)}>Remove</button>
+                <button onClick={() => editItemViaAlertInput(item.id)}>Edit</button>
+                </div>
+                <div className='dependencies-tab'>
+                {/* select to add dependency using other items name as option and adding by their id */}
+                {/* list the dependencies of this item, searching in all lists by id */}
+                <div className='dependencies-list'>
+                    {item.dependencies.map(dependency => (
+                    <div key={dependency} className="dependency">
+                      {findDependencyName(dependency)}
+                    </div>
+                    ))}
+                </div>
+                <div className='adddelete'>
+                    <div>
+                      ADD
+                      <select onChange={(e) => addDependency(item.id, e.target.value)}>
+                          {/* list every single item in the list every ucfr */}
+                          <option value="null">Select</option>
+                          {ucfrs.ucfrs.map((model, i) => (
+                          model.list.map(item => (
+                          <option key={item.id} value={item.id}>{model.type +'-'+ model.category +'| '+item.name}</option>
+                          ))
+                          ))}
+                      </select>
+                      </div>
+                      <div>
+                          DELETE
+                          <select onChange={(e) => removeDependency(item.id, e.target.value)}>
+                            {/* list every single dependency of this item as option */}
+                            <option value="null">Select</option>
+                            {item.dependencies.map(dependency => (
+                            <option key={dependency} value={dependency}>{findDependencyName(dependency)}</option>
+                            ))}
+                          </select>
+                      </div>
+                </div>
+                </div>
+              </div>
+          </div>
+          ))}
+        </div>
+    </div>
+    </div>
+    <div>
+    <button onClick={saveUcfrsInLocalStorage}>Save</button>
+    <button onClick={loadUcfrsFromLocalStorage}>Load</button>
+    <button onClick={downloadUcfrsAsJson}>Download</button>
+    </div>
+    <input type="text" onChange={(e) => {setJsonInsert(e.target.value)}}/>
+    <button onClick={insertucfrsFromJson}>Insert</button>
+  </div>
+);
+}
