964b74d852c552d680b0d98770fe0cc2c826e8a1
mikhael-dantas <mikhael-dcs@outlook.com>
2022-08-12 16:46:46 -0300

    added popup context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 import { AppProps } from 'next/app'
 import '../style.css';
-import { UcfrsProvider } from '../ucfrsContext';
+import { UcfrsProvider } from '../UcfrsContext';
+import { PopupProvider } from '../PopupContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <UcfrsProvider>
-        <Component {...pageProps} />
-      </UcfrsProvider>
+      <PopupProvider>
+        <UcfrsProvider>
+          <Component {...pageProps} />
+        </UcfrsProvider>
+      </PopupProvider>
     </ChakraProvider>
   )
 }
