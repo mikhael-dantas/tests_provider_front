6a24f4dccb8f676f6fa0729862048e7b3fb5ef90
mikhael-dantas <mikhael-dcs@outlook.com>
2022-08-12 16:45:51 -0300

    adjusting ucfrlists context

--- src/UcfrsContext.tsx
+++ src/UcfrsContext.tsx
@@ -44,8 +44,11 @@ export interface IUcfrLists {
    functionalRequirements: IFunctionalRequirement[]
 }
 
-const ucfrListsContext = React.createContext<IUcfrLists>(undefined)
-const updateUcfrListsContext = React.createContext<React.Dispatch<React.SetStateAction<IUcfrLists>>>(undefined)
+const ucfrListsContext = React.createContext<IUcfrLists>(null)
+const updateUcfrListsContext = React.createContext<React.Dispatch<React.SetStateAction<IUcfrLists>>>(null)
+
+const currentModuleContext = React.createContext<IModule>(null)
+const updateCurrentModuleContext = React.createContext<React.Dispatch<React.SetStateAction<IModule>>>(null)
 
 export function UcfrsProvider({children}) {
    const [ucfrLists, setUcfrLists] = React.useState<IUcfrLists>({
@@ -55,21 +58,36 @@ export function UcfrsProvider({children}) {
       nestedUseCases: [],
       functionalRequirements: []
    })
+
+   const [currentModule, setCurrentModule] = React.useState<IModule | undefined>(undefined)
+
    return (
       <ucfrListsContext.Provider value={ucfrLists}>
       <updateUcfrListsContext.Provider value={setUcfrLists}>
-         {children}
+         <currentModuleContext.Provider value={currentModule}>
+         <updateCurrentModuleContext.Provider value={setCurrentModule}>
+            {children}
+         </updateCurrentModuleContext.Provider>
+         </currentModuleContext.Provider>
       </updateUcfrListsContext.Provider>
       </ucfrListsContext.Provider>
    )
 }
 
-export function useUcfrLists() {
+export function useUcfrListsContext() {
    const ucfrLists = React.useContext(ucfrListsContext)
    return ucfrLists
 }
-
-export function useUpdateUcfrLists() {
+export function useUpdateUcfrListsContext() {
    const setUcfrLists = React.useContext(updateUcfrListsContext)
    return setUcfrLists
+}
+
+export function useCurrentModuleContext() {
+   const currentModule = React.useContext(currentModuleContext)
+   return currentModule
+}
+export function useUpdateCurrentModuleContext() {
+   const setCurrentModule = React.useContext(updateCurrentModuleContext)
+   return setCurrentModule
 }
