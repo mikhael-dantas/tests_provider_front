5f1cf348375e07182e361f0596f3755e2bcf1a78
mikhael-dantas <mikhael-dcs@outlook.com>
2022-08-16 16:34:56 -0300

    made use cases container with functional drag'ndrop and the list

---
+++ src/components/ucfrLists/useCases/AddUseCase.tsx
@@ -0,0 +1,58 @@
+import { Grid } from "@chakra-ui/react"
+import React from "react"
+import { useCurrentModuleContext, useUcfrListsContext, useUpdateUcfrListsContext } from "../../../UcfrsContext"
+import { GenerateUUID } from "../../../utils/UUIDGenerator"
+
+export default function AddUseCase() {
+   const ucfrListsFromContext = useUcfrListsContext()
+   const updateUcfrListsFromContext = useUpdateUcfrListsContext()
+
+   const currentModuleFromContext = useCurrentModuleContext()
+
+   const [usecaseAddInput, setUsecaseAddInput] = React.useState<string>("")
+
+   const usecaseAddInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUsecaseAddInput(e.target.value)
+   }
+
+   const usecaseAddHandler = () => {
+      const moduleId = currentModuleFromContext.id
+
+      updateUcfrListsFromContext({
+         ...ucfrListsFromContext,
+         modules: ucfrListsFromContext.modules.map(module => {
+            if (module.id === moduleId) {
+               return {
+                  ...module,
+                  useCases: [
+                     ...module.useCases,
+                     {
+                        id: GenerateUUID(),
+                        name: usecaseAddInput,
+                        moduleId: moduleId,
+                        completed: false,
+                        neededFrsToWorkIds: [],
+                        tagIds: [],
+                        usecasesPipelineIds: [],
+                     }
+                  ]
+               }
+            }
+            return module
+         }),
+      })
+
+      setUsecaseAddInput("")
+   }
+
+   return (
+      <Grid className={'ucfrsListContainer'} 
+         templateColumns={'3fr 1fr'}
+         width={'100%'}
+         height={'2rem'}
+      >
+         <input className={'input'} type={'text'} value={usecaseAddInput} onChange={usecaseAddInputHandler} placeholder={'Add a usecase'} />
+         <button className={'button'} onClick={usecaseAddHandler}>Add</button>
+      </Grid>
+   )
+}
