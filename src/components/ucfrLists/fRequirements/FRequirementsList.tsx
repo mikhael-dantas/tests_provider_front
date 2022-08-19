02fcd49c1396752bb1193d72c1fba0650e3ec3a8
mikhael-dantas <mikhael-dcs@outlook.com>
2022-08-19 17:51:49 -0300

    name fix

--- src/components/ucfrLists/fRequirements/FRequirementsList.tsx
+++ src/components/ucfrLists/fRequirements/FRequirementsList.tsx
@@ -87,7 +87,7 @@ export default function FRequirementList() {
                }
                return {
                   ...m,
-                  FRequirements: newFRequirements
+                  functionalRequirements: newFRequirements
                }
             }
             )
