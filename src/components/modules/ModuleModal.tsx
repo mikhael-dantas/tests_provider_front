7dd4f7ecc8355aa6cc6fd9b92cbc8018f99cad5f
Mikhael Dantas <mikhael-dcs@outlook.com>
2022-09-01 12:44:51 -0300

    inserted confirmation generic modal

--- src/components/modules/ModuleModal.tsx
+++ src/components/modules/ModuleModal.tsx
@@ -1,56 +1,66 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Input, ModalFooter, Button } from '@chakra-ui/react'
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Input, ModalFooter, Button, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
 import { customTheme } from '../../theme'
-import { IModule, useUcfrListsContext, useUpdateUcfrListsContext } from '../../UcfrsContext'
+import { IModule, useCurrentModuleContext, useUcfrListsContext, useUpdateUcfrListsContext } from '../../UcfrsContext'
+import ConfirmationModal from '../ConfirmationModal'
 
 function ModuleModal({isOpen, onClose, module: receivedModule}: 
     {isOpen: boolean, onClose: () => void, module: IModule}
     ) {
-        const ucfrListsFromContext = useUcfrListsContext()
-        const updateUcfrListsFromContext = useUpdateUcfrListsContext()
+    const ucfrListsFromContext = useUcfrListsContext()
+    const updateUcfrListsFromContext = useUpdateUcfrListsContext()
+
+    const currentModuleFromContext = useCurrentModuleContext()
 
 
-        const [moduleEditNameInput, setModuleEditNameInput] = React.useState<string>("")
+    const [moduleEditNameInput, setModuleEditNameInput] = React.useState<string>("")
 
-        const moduleEditNameInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-            setModuleEditNameInput(e.target.value)
+    const moduleEditNameInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setModuleEditNameInput(e.target.value)
+    }
+
+    const moduleEditNameHandler = () => {
+        if (moduleEditNameInput === '') {
+            return
         }
 
-        const moduleEditNameHandler = () => {
-            if (moduleEditNameInput === '') {
-                return
-            }
+        const moduleToEdit = ucfrListsFromContext.modules.find(m => m.id === receivedModule.id)
 
-            const moduleToEdit = ucfrListsFromContext.modules.find(m => m.id === receivedModule.id)
+        if (!moduleToEdit) {
+            console.log(`moduleToEdit not found: ${receivedModule.id}`)
+            return
+        }
 
-            if (!moduleToEdit) {
-                console.log(`moduleToEdit not found: ${receivedModule.id}`)
-                return
-            }
+        const newModule = {
+            ...moduleToEdit,
+            name: moduleEditNameInput,
+        }
 
-            const newModule = {
-                ...moduleToEdit,
-                name: moduleEditNameInput,
-            }
-
-            updateUcfrListsFromContext({
-                ...ucfrListsFromContext,
-                modules: ucfrListsFromContext.modules.map(m => {
-                    if (m.id === newModule.id) {
-                        return newModule
-                    }
-                    return m
+        updateUcfrListsFromContext({
+            ...ucfrListsFromContext,
+            modules: ucfrListsFromContext.modules.map(m => {
+                if (m.id === newModule.id) {
+                    return newModule
                 }
-                )
-            })
+                return m
+            }
+            )
+        })
+    }
+
+    const deleteModuleHandler = () => {
+        if (currentModuleFromContext?.id === receivedModule.id) {
+            console.log('Cannot delete current module')
+            return
         }
 
-        const deleteModuleHandler = () => {
-            updateUcfrListsFromContext({
-                ...ucfrListsFromContext,
-                modules: ucfrListsFromContext.modules.filter(m => m.id !== receivedModule.id)
-            })
-        }
+        updateUcfrListsFromContext({
+            ...ucfrListsFromContext,
+            modules: ucfrListsFromContext.modules.filter(m => m.id !== receivedModule.id)
+        })
+    }
+
+    const { isOpen: secondIsOpen, onOpen: secondOnOpen, onClose: secondOnClose } = useDisclosure()
 
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
@@ -63,7 +73,7 @@ function ModuleModal({isOpen, onClose, module: receivedModule}:
                     style={{backgroundColor: 'white', color: 'black'}}
                     value={moduleEditNameInput} onChange={moduleEditNameInputHandler} />
                     <button onClick={moduleEditNameHandler}>Save name</button>
-                    <button onClick={deleteModuleHandler}>Delete module</button>
+                    <button onClick={secondOnOpen}>Delete module</button>
                 </ModalBody>
 
                 <ModalFooter>
@@ -72,6 +82,7 @@ function ModuleModal({isOpen, onClose, module: receivedModule}:
                     </Button>
                 </ModalFooter>
             </ModalContent>
+            <ConfirmationModal isOpen={secondIsOpen} onClose={secondOnClose} callback={deleteModuleHandler}/>
         </Modal>
     )
 }
