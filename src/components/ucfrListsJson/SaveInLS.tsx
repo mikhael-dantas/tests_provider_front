7ee4120789bdca3b2aeef34500a09121a5902abe
Mikhael Dantas <mikhael-dcs@outlook.com>
2022-09-19 13:16:18 -0300

    confirmation added to save in localStorage

--- src/components/ucfrListsJson/SaveInLS.tsx
+++ src/components/ucfrListsJson/SaveInLS.tsx
@@ -1,3 +1,5 @@
+import { useDisclosure } from '@chakra-ui/react'
+import ConfirmationModal from '@myComponents/ConfirmationModal'
 import { useUcfrListsContext } from '@myContexts/UcfrsContext'
 import React from 'react'
 
@@ -6,8 +8,22 @@ function SaveInLS() {
     const saveInLS = () => {
         localStorage.setItem('ucfrLists', JSON.stringify(ucfrListsFromContext))
     }
+
+    const { 
+        isOpen: isOpenSaveInLSConfirmation,
+        onOpen: onOpenSaveInLSConfirmation,
+        onClose: onCloseSaveInLSConfirmation
+    } = useDisclosure()
     return (
-        <button className='button' onClick={saveInLS}>Save JSON locally</button>
+        <>
+            <button className='button' onClick={
+                () => { 
+                    onOpenSaveInLSConfirmation()
+                }
+            }>Save JSON locally</button>
+
+            <ConfirmationModal callback={() => {saveInLS();onCloseSaveInLSConfirmation()}} isOpen={isOpenSaveInLSConfirmation} onClose={onCloseSaveInLSConfirmation} />
+        </>
     )
 }
 
